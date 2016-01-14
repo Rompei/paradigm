726a36943e63cdce514729aede43bbafbee7ec37
Rompei <rompei0924@gmail.com>
2016-01-14 22:03:35 +0900

    Get data from github repogitory.

--- static/js/main.js
+++ static/js/main.js
@@ -4,27 +4,84 @@ $(function(){
 
   var langName = $.queryParameter("lang");
   if(langName == null){
-    $(location).attr("href", "/");
+    returnRoot();
   }
 
-  var svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height)
-    .attr("transform","translate(50,50)");//ここがツリーの左上になる。
+  var rompei = new Gh3.User("Rompei")
+    , pdmcrawler = new Gh3.Repository("paradigm-crawler", rompei);
 
-  var tree = d3.layout.tree()
-    .size([2048,2840]) // .size()でツリー全体のサイズを決める。
-    .children(children); // children()で入れ子のための名前を指定する。
-
-  function children(d) {
-    return d["descendents"];
-  }
-
-  d3.json("../data/" + langName + ".json", function(data) {
-    if(data == null){
-      $(location).attr("href", "/");
+  pdmcrawler.fetch(function(err, res){
+    if(err){
+      console.log(err);
+      returnRoot();
     }
+    pdmcrawler.fetchBranches(function(err, res){
+      if(err){
+        console.log(err);
+        returnRoot();
+      }
+      var master = pdmcrawler.getBranchByName("master");
+      if(!master)
+        returnRoot();
+      master.fetchContents(function(err, res){
+        if(err){
+          console.log(err);
+          returnRoot();
+        }
 
+        var dataDir = master.getDirByName("data");
+        if(!dataDir)
+          returnRoot();
+        dataDir.fetchContents(function(err, res){
+          if(err){
+            console.log(err);
+            returnRoot();
+          }
+          var file = dataDir.getFileByName(langName + ".json");
+          if(!file)
+            returnRoot();
+          file.fetchContent(function(err, res){
+            if(err){
+              console.log(err);
+              returnRoot();
+            }
+            var json = $.evalJSON(file.getRawContent());
+            setupTree(json.languageTree);
+            addTitle(json.languageTree.name);
+            addUpdated(json.time);
+          });
+        });
+      });
+    });
+  });
+
+  function returnRoot(){
+    $(location).attr("href", "/");
+    return;
+  }
+
+  function addTitle(title){
+    $('.langName').text(title);
+  }
+
+  function addUpdated(updated){
+    var date = new Date(updated);
+    $('.updated').text('Captured at ' + date);
+  }
+
+  function setupTree(data){
+    var svg = d3.select("body").append("svg")
+      .attr("width", width)
+      .attr("height", height)
+      .attr("transform","translate(50,50)");//ここがツリーの左上になる。
+
+    var tree = d3.layout.tree()
+      .size([2048,2840]) // .size()でツリー全体のサイズを決める。
+      .children(children); // children()で入れ子のための名前を指定する。
+
+    function children(d) {
+      return d["descendents"];
+    }
     var nodes = tree.nodes(data);
 
     var links = tree.links(nodes);
@@ -56,6 +113,6 @@ $(function(){
       .attr("stroke", "red")
       .attr("d",diagonal);
 
-  });
+  }
 });
 
